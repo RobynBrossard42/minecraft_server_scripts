6ff66863250e9dcb7292a449ab290b642af4e27c
Robyn Brossard <robyn.brossard@icloud.com>
2025-03-05 00:40:53 -0800

    changed the timer on shutting down the server to 5 minutes

--- stop.sh
+++ stop.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#echo ""say The server will shut down in 1 minute!!!\r\nTry not to be opening containers, traveling between dimensions, riding an entity, or traveling at high velocity at that time\r\nThank you for your patience!"" > /run/minecraft.stdin
-#sleep 4m
-
-echo "say The server will shut down in 1 minute!!!" > /run/minecraft.stdin
+echo "say The server will shut down in 5 minutes!!!" > /run/minecraft.stdin
 echo "say Try not to be opening containers, traveling between dimensions," > /run/minecraft.stdin
 echo "say riding an entity, or traveling at high velocity at that time" > /run/minecraft.stdin
 echo "say Thank you for your patience!" > /run/minecraft.stdin
+
+sleep 4m
+
+echo "say The server will shut down in 1 minute!!!" > /run/minecraft.stdin
 
 sleep 55
 
